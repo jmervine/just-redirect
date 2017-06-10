23370717d3b1692da2216ff47a45c4a205f99eab
Josh Mervine <joshua@mervine.net>
2017-06-10 11:35:22 -0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# just-redirect
+
+Set `REDIRECT_TARGET`, start it, hit it and it'll redirect...
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+var target, port, bind string
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, target, 301)
+}
+
+func main() {
+	target = os.Getenv("REDIRECT_TARGET")
+	if target == "" {
+		panic("REDIRECT_TARGET required")
+	}
+
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	bind = os.Getenv("BIND")
+
+	http.HandleFunc("/", redirect)
+	listen := bind + ":" + port
+	print("listening on: " + listen + "\n")
+	err := http.ListenAndServe(listen, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
